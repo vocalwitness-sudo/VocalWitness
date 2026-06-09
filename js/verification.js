cae13284ba523795145f4e388b6e655e68b466ee
LawMann <vocalwitness1@gmail.com>
2026-06-09 02:09:35 +0100

    Update verification.js

--- js/verification.js
+++ js/verification.js
@@ -2,6 +2,8 @@
  * VocalWitness Verification Module
  * Manages Identity Tiering, ZK Proofs, and Peer Invitations
  *//
+
+import exifr from 'https://cdn.jsdelivr.net/npm/exifr@7.1.3/dist/exifr.esm.js';
 import { collection, addDoc, doc, getDoc, updateDoc, setDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { db, auth } from "./firebase-config.js";
 import { showToast } from "./utils.js";
