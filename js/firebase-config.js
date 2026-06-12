edc9923c1333c181993180e5049f782471307c91
LawMann <vocalwitness1@gmail.com>
2026-06-12 18:39:31 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,22 +1,22 @@
 // js/firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
+// js/firebase-config.js
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
+import { getAuth, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyATxYekxgjdLP2SfR42FG8rEdajq_pIEb0", 
+    apiKey: "AIzaSyATxYekXgjdLP2SfR42FG8rEdajq_pIEb0",
     authDomain: "vocalwitness-3affa.firebaseapp.com",
     projectId: "vocalwitness-3affa",
-    storageBucket: "vocalwitness-3affa.appspot.com",
+    storageBucket: "vocalwitness-3affa.firebasestorage.app",
     messagingSenderId: "108466981866",
     appId: "1:108466981866:web:b53360ad44012a576c8093"
 };
 
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
 export const storage = getStorage(app);
 export const provider = new GoogleAuthProvider();
 // In js/verification.js
