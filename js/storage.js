45106b42d466868f24d32e75f90a413224fe4527
LawMann <vocalwitness1@gmail.com>
2026-06-14 05:20:26 +0100

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,15 +1,6 @@
 // js/storage.js
-import { storage } from "./firebase-config.js";
-import { ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-storage.js";
-import { showToast } from "./utils.js";
-
-// This is the SINGLE top-level export
 export const state = {
     user: null,
-    isWitnessVerified: false
+    isWitnessVerified: false,
+    tier: 'citizen'
 };
-
-// Storage functions
-export async function uploadToStorage(file, folder) {
-    // ... your storage logic
-}
