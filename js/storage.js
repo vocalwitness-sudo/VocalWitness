6cb234c32362f6975922fee9331f338a21fdad86
LawMann <vocalwitness1@gmail.com>
2026-06-15 22:04:04 +0100

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,6 +1,26 @@
-// js/storage.js
+// js/storage.js - Reactive State
 export const state = {
     user: null,
     isWitnessVerified: false,
-    tier: 'citizen'
+    tier: 'citizen',
+    language: 'en'
 };
+
+// Reactive Update Helper
+export function updateState(newData) {
+    Object.assign(state, newData);
+
+    // Broadcast change to all listeners
+    window.dispatchEvent(new CustomEvent('state-changed', { 
+        detail: { state } 
+    }));
+}
+
+// Specific helper for user
+export function updateUser(user) {
+    updateState({
+        user: user,
+        isWitnessVerified: !!user,
+        tier: user ? 'witness' : 'citizen'
+    });
+}
