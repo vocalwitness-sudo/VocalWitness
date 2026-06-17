f9f5343bbcc8351fe92b800d42bfbd976f9e2b35
LawMann <vocalwitness1@gmail.com>
2026-06-17 20:43:56 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
 const CACHE_NAME = 'vocalwitness-v5'; // Increased version
 
 const STATIC_ASSETS = [
-   const STATIC_ASSETS = [
-    '/VocalWitness/',
+   /VocalWitness/',
     '/VocalWitness/index.html',
     '/VocalWitness/manifest.json',
     '/VocalWitness/style.css',
@@ -16,8 +15,6 @@ const STATIC_ASSETS = [
     '/VocalWitness/js/utils.js',
     '/VocalWitness/js/i18n.js',
     '/VocalWitness/js/firebase-config.js'
-    // Removed '/VocalWitness/js/sw.js'
-]; // Self-reference
 ];
 
 // Install Event
