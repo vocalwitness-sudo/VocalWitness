1694e14b0f8ff38844be60130ed046b110e9933f
LawMann <vocalwitness1@gmail.com>
2026-06-19 21:14:45 +0100

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,24 +1,18 @@
-// Import the functions you need from the SDKs
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-storage.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-storage.js";
 
-// Your Firebase configuration object
 const firebaseConfig = {
-  apiKey: "AIzaSyATxYekXgjdLP2SfR42FG8rEdajq_pIEb0",
-  authDomain: "vocalwitness-3affa.firebaseapp.com",
-  projectId: "vocalwitness-3affa",
-  storageBucket: "vocalwitness-3affa.firebasestorage.app",
-  messagingSenderId: "108466981866",
-  appId: "1:108466981866:web:b53360ad44012a576c8093",
-  measurementId: "G-LQ2JRWD06W"
+    apiKey: "AIzaSyATxYekXgjdLP2SfR42FG8rEdajq_pIEb0",
+    authDomain: "vocalwitness-3affa.firebaseapp.com",
+    projectId: "vocalwitness-3affa",
+    storageBucket: "vocalwitness-3affa.firebasestorage.app",
+    messagingSenderId: "108466981866",
+    appId: "1:108466981866:web:b53360ad44012a576c8093"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export these so you can use them in other files
+export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-export const auth = getAuth(app);
