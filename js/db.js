f38c3a20372b5a017480f7479e0836fe2e2400df
LawMann <vocalwitness1@gmail.com>
2026-06-21 10:32:58 +0100

    Update db.js

--- js/db.js
+++ js/db.js
@@ -6,3 +6,13 @@ export const dbPromise = openDB('VocalWitnessDB', 1, {
     db.createObjectStore('offline-posts', { keyPath: 'id', autoIncrement: true });
   },
 });
+
+import { enableIndexedDbPersistence } from "firebase/firestore";
+// Call this before your first read/write
+enableIndexedDbPersistence(db).catch((err) => {
+    if (err.code == 'failed-precondition') {
+        console.warn("Multiple tabs open, persistence disabled.");
+    } else if (err.code == 'unimplemented') {
+        console.warn("Browser doesn't support persistence.");
+    }
+});
