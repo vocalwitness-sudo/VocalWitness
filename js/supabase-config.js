5ad8654f0262f74c238f4fd5f371b60c1ad58488
LawMann <vocalwitness1@gmail.com>
2026-07-27 21:07:28 +0100

    Delete js/supabase-config.js

--- js/supabase-config.js
+++
@@ -1,7 +0,0 @@
-// js/supabase-config.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-const SUPABASE_URL = 'https://opotzbvjlgykgyjjwxwb.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_P_UfxwkVBkM6XdzHb8uiFw_EJGGh7hA';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
