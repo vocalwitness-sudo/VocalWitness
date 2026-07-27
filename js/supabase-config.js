bdc4701652abb1a098ba5c61864aa5c321aa7252
LawMann <vocalwitness1@gmail.com>
2026-07-27 18:10:04 +0100

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -5,4 +5,3 @@ const SUPABASE_URL = 'https://opotzbvjlgykgyjjwxwb.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_P_UfxwkVBkM6XdzHb8uiFw_EJGGh7hA';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-import { createClient } from 'https://unpkg.com/@supabase/supabase-js@2/dist/module/index.js';
