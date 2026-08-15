431f9a61063d01b6d3332b8af5edc1b1d9ee2cc1
LawMann <vocalwitness1@gmail.com>
2026-08-15 06:46:19 +0100

    Update vite.config.js

--- js/vite.config.js
+++ js/vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   root: './',
+
+  plugins: [
+    tailwindcss(),
+  ],
 
   build: {
     outDir: 'public',
