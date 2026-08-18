5b940f714d1650399b2f7fcd3d588ef73177092e
LawMann <vocalwitness1@gmail.com>
2026-08-18 17:04:18 +0100

    Update localization.js

--- localization.js
+++ localization.js
@@ -1,33 +1,9 @@
-const translations = {
-    "en": { /* your English JSON */ },
-    "ar": { /* your Arabic JSON */ },
-    "fr": { /* your French JSON */ },
-    // ... etc
-};
+// localization.js - Bridge to Hardened i18n Engine
+import { t, loadTranslations, updateUILanguage, initLanguage } from './js/i18n.js';
 
+export { t, updateUILanguage, initLanguage };
+
+// Map setLanguage to loadTranslations so language selectors update everything instantly
 export function setLanguage(langCode) {
-    const activeData = translations[langCode] || translations['en'];
-    // Update all UI elements by class or ID
-    document.querySelectorAll('[data-i18n]').forEach(element => {
-        const key = element.getAttribute('data-i18n');
-        element.innerText = activeData[key];
-    });
+    loadTranslations(langCode);
 }
-
-export function updateUILanguage(langCode) {
-    document.documentElement.lang = langCode;
-    const activeData = translations[langCode] || translations['en'];
-    
-    // Update innerText
-    document.querySelectorAll('[data-i18n]').forEach(el => {
-        const key = el.getAttribute('data-i18n');
-        if (activeData[key]) el.innerText = activeData[key];
-    });
-
-    // Update Placeholders
-    document.querySelectorAll('[data-i18n-placeholder]').forEach(el => {
-        const key = el.getAttribute('data-i18n-placeholder');
-        if (activeData[key]) el.placeholder = activeData[key];
-    });
-}
-
