33c0884853d5b120990b2d5e4a236d4402187e96
LawMann <vocalwitness1@gmail.com>
2026-09-15 22:31:31 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./js/**/*.js",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
